aabef76e225450d8b744eb99b483a21d0f835b95
Elvin Nilsson Ribbing <elvin.nilssonribbing@elev.ga.lbs.se>
2019-01-14 10:18:51 +0100

    Examination - Introduktion till Unity

---
+++ Assets/movement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float movespeed = 0.66f;
+    public float turnspeed = 60f;
+    public SpriteRenderer rend;
+    public Color col;
+    int timer = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        movespeed = Random.Range(1, 2.5f);
+        InvokeRepeating("Timer", 1, 1);
+    }
+
+    void Timer()
+    {
+        Debug.Log("Timer:" + timer * Time.deltaTime);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movespeed += 0.00001f * timer;
+        turnspeed += 0.0001f * timer;
+        timer += 1;
+        transform.Translate(Vector2.up * movespeed * Time.deltaTime);
+
+        if (movespeed > 8)
+        {
+            movespeed = 8;
+        }
+
+        if (turnspeed > 167)
+        {
+            turnspeed = 167;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+            {
+                movespeed /= 2;
+            }
+
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                movespeed *= 2;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, 0, -turnspeed * Time.deltaTime);
+                rend.color = new Color(0, 0, 1);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0, 0, turnspeed * Time.deltaTime * 0.9f);
+                rend.color = new Color(0, 1, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+            rend.color = new Color(Random.value, Random.value, Random.value);
+            }
+    }
+}
---
+++ Assets/wrapping.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wrapping : MonoBehaviour
+{
+    Renderer renderer;   // Här döps variabeln för renderern (det som gör alla objekt synliga) till "renderer".     
+
+    // Use this for initialization
+    void Start()
+    {
+        var cam = Camera.main;
+
+        var viewportPosition = cam.WorldToViewportPoint(transform.position);
+
+        var newPosition = transform.position;
+
+        transform.position = new Vector3(Random.Range(-11, 11), Random.Range(-4.5f, 4.5f), 0);
+
+        renderer = GetComponent<Renderer>();
+    }
+
+    bool isWrappingX = false;
+    bool isWrappingY = false;
+
+    bool CheckRenderers = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cam = Camera.main;
+
+        var viewportPosition = cam.WorldToViewportPoint(transform.position);
+
+        var newPosition = transform.position;
+
+        var isVisible = CheckRenderers;
+
+        if (renderer.isVisible)
+        {
+            CheckRenderers = true;
+        }
+        else
+        {
+            CheckRenderers = false;
+        }
+
+        if (isVisible)
+        {
+            isWrappingX = false;
+            isWrappingY = false;
+        }
+
+
+
+        if (!isWrappingX & (viewportPosition.x > 1 || viewportPosition.x < 0))
+        {
+            newPosition.x = -(newPosition.x);
+            isWrappingX = true;
+        }
+
+        if (!isWrappingY & (viewportPosition.y > 1 || viewportPosition.y < 0))
+        {
+            newPosition.y = -(newPosition.y);
+            isWrappingY = true;
+        }
+
+        transform.position = newPosition;
+    }
+}
